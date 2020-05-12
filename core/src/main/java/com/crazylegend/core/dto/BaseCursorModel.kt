d0afb540b4a12de9479e1129522f8ea777029554
CraZyLegenD <crazyxylene@gmail.com>
2020-05-12 20:39:56 +0200

    moved thumbnail to base class

--- core/src/main/java/com/crazylegend/core/dto/BaseCursorModel.kt
+++ core/src/main/java/com/crazylegend/core/dto/BaseCursorModel.kt
@@ -1,7 +1,13 @@
 package com.crazylegend.core.dto
 
+import android.content.ContentResolver
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.net.Uri
+import android.os.Build
 import android.os.Parcelable
+import android.provider.MediaStore
+import android.util.Size
 import kotlinx.android.parcel.Parcelize
 import java.util.*
 import java.util.concurrent.TimeUnit
@@ -23,6 +29,27 @@ open class BaseCursorModel(
         open val width: Int?,
         open val height: Int?
 ) : Parcelable {
+
+
+    /**
+     * Or just use Glide with the contentUri
+     * @param contentResolver ContentResolver
+     * @param customSize Size
+     * @param options Options?
+     * @return Bitmap?
+     */
+    @Suppress("DEPRECATION")
+    fun loadThumbnail(contentResolver: ContentResolver,
+                      customSize: Size = Size(size ?: 150, size ?: 150),
+                      legacyKind:Int = MediaStore.Video.Thumbnails.MICRO_KIND,
+                      options: BitmapFactory.Options? = null): Bitmap? {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            contentResolver.loadThumbnail(contentUri, customSize, null)
+        } else {
+            MediaStore.Video.Thumbnails.getThumbnail(contentResolver, id, legacyKind,
+                    options ?: BitmapFactory.Options())
+        }
+    }
 
     val extension get() = displayName?.substringAfterLast(".")
 
--- videopicker/src/main/java/com/crazylegend/videopicker/videos/VideoModel.kt
+++ videopicker/src/main/java/com/crazylegend/videopicker/videos/VideoModel.kt
@@ -1,13 +1,7 @@
 package com.crazylegend.videopicker.videos
 
-import android.content.ContentResolver
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.net.Uri
-import android.os.Build
 import android.os.Parcelable
-import android.provider.MediaStore
-import android.util.Size
 import com.crazylegend.core.dto.BaseCursorModel
 import kotlinx.android.parcel.Parcelize
 
@@ -38,22 +32,4 @@ data class VideoModel(
 ) : BaseCursorModel(id, displayName, dateAdded, contentUri, dateModified, description, size, width, height), Parcelable {
 
 
-    /**
-     * Or just use Glide with the contentUri
-     * @param contentResolver ContentResolver
-     * @param customSize Size
-     * @param options Options?
-     * @return Bitmap?
-     */
-    @Suppress("DEPRECATION")
-    fun loadThumbnail(contentResolver: ContentResolver,
-                      customSize: Size = Size(size ?: 150, size ?: 150),
-                      options: BitmapFactory.Options? = null): Bitmap? {
-        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            contentResolver.loadThumbnail(contentUri, customSize, null)
-        } else {
-            MediaStore.Video.Thumbnails.getThumbnail(contentResolver, id, MediaStore.Video.Thumbnails.MICRO_KIND,
-                    options ?: BitmapFactory.Options())
-        }
-    }
 }
